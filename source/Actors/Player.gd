0c025ba1ac0e09f2681a08e0873ec5eb4a48d6ee
Thomas42705 <61979110+Thomas42705@users.noreply.github.com>
2020-03-26 16:37:05 -0400

    moving

--- source/Actors/Player.gd
+++ source/Actors/Player.gd
@@ -1,2 +1,14 @@
 extends Actor
 
+
+func _physics_process(delta: float) -> void:
+	var direction: = get_direction()
+	velocity = speed * direction
+	velocity = move_and_slide(velocity)
+	
+	
+	func get_direction() -> Vector2:
+		return Vector2(
+		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
+			-1.0 if Input.is_action_just_pressed("jump") and is_on_floor()else 1.0
+	)
