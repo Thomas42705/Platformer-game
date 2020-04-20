a76007506580f19630b04a71f5ee64cc1104507d
Thomas42705 <61979110+Thomas42705@users.noreply.github.com>
2020-04-20 13:41:39 -0400

    stuck

--- source/Actors/Player.gd
+++ source/Actors/Player.gd
@@ -8,17 +8,17 @@ func _physics_process(delta: float) -> void:
 	
 	
 func get_direction() -> Vector2:
-		return Vector2(
+	return Vector2(
 		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
 		-1.0 if Input.is_action_just_pressed("jump") and is_on_floor()else 1.0
 	)
 
 
 func calculate_move_velocity(
-linear_velocity: Vector2,
-speed: Vector2,
-direction: Vector2
-) -> Vector2:
+		linear_velocity: Vector2,
+		speed: Vector2,
+		direction: Vector2
+	) -> Vector2:
 	var new_velocity = linear_velocity
 	new_velocity.x = speed.x * direction.x
 	new_velocity.y += gravity * get_physics_process_delta_time()
@@ -33,3 +33,4 @@ direction: Vector2
 #April 7 2020
 #April 8 2020 
 #April 9 2020
+#April 20 2020
