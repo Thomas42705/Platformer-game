d84424ad478de0132a17f37793c54eb034977372
Thomas42705 <61979110+Thomas42705@users.noreply.github.com>
2020-05-11 11:14:40 -0400

    enemy death and movment

--- source/Actors/Enemy.gd
+++ source/Actors/Enemy.gd
@@ -2,7 +2,15 @@ extends "res://source/Actors/Actor.gd"
 
 
 func _ready() -> void:
+	set_physics_process(false)
 	_velocity.x = -speed.x
+
+
+func _on_StompDetector_body_entered(body: PhysicsBody2D) -> void:
+	if body.global_position.y > get_node("StompDetector").global_position.y:
+		return
+	get_node("CollisionShape2D").disabled = true
+	queue_free()
 
 
 func _physics_process(delta: float) -> void:
@@ -10,4 +18,7 @@ func _physics_process(delta: float) -> void:
 	if is_on_wall():
 		_velocity.x *= -1.0
 	_velocity.y = move_and_slide(_velocity, FLOOR_NORMAL).y
+
+
+
 
